57eafe152ef6d7ceea3fddd32ad6c4c60995fd3b
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-05-12 17:56:23 +0200

    added emoji

--- components/Live.tsx
+++ components/Live.tsx
@@ -4,12 +4,26 @@ import { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
 import { CursorMode, CursorState, Reaction } from "@/types/type";
 import ReactionSelector from "./reaction/ReactionButton";
+import FlyingReaction from "./reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updatemyPresece] = useMyPresence() as any;
     const [cursorState, setCursorState] = useState<CursorState>({ mode: CursorMode.Hidden });
     const [reactions, setReaction] = useState<Reaction[]>([]);
+    useInterval(() => {
+        if(cursorState.mode === CursorMode.Reaction && cursorState.isPressed && cursor){
+            setReaction((reactions) => reactions.concat( [
+            
+                {
+                    point: cursor,
+                    timestamp: Date.now(),
+                    value: cursorState.reaction
+                }
+            ]));
+        }
+    }, 100);
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
@@ -44,7 +58,7 @@ const Live = () => {
     const handlePointerUp = useCallback((e: React.PointerEvent) => {
        
 
-        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: false } : state)
+        setCursorState((state: CursorState) => cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state)
 
     }, [cursorState.mode, setCursorState]);
 
@@ -91,6 +105,16 @@ const Live = () => {
         >
             <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
 
+            {reactions.map((r) => (
+                <FlyingReaction
+                key={r.timestamp.toString()}
+                x={r.point.x}
+                y={r.point.y}
+                timestamp={r.timestamp}
+                value={r.value}
+                />
+            ))}
+
             {cursor && (
                 <CursorChat 
                 cursor = {cursor}
@@ -103,6 +127,9 @@ const Live = () => {
             {cursorState.mode === CursorMode.ReactionSelector && (
                 <ReactionSelector
                 setReaction={setReactions}
+            
+                    
+                
                 />
             )}
 
