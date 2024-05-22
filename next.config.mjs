a3bd8de4d7b5787e62af35c6c162013d4bb49020
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-05-22 21:08:37 +0200

    Modified config for deploying on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({"utf-8-validate": "commonjs utf-8-validate",
+    "bufferutil": "commonjs bufferutil",
+    canvas: "commonjs canvas",
+  });
+    return config;
+  },
     images: {
         remotePatterns: [
           {
             protocol: "https",
             hostname: "liveblocks.io",
             port: "",
-          },
-        ],
+          }
+        ]
       },
-       
+       typescript: {
+        ignoreBuildErrors: true,
+       },
 
 };
 
